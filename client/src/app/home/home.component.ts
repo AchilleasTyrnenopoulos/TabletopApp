a6e60b3d5e22ce35161e01ac576722e6b18cc056
AchilleasTyrnenopoulos <68752518+AchilleasTyrnenopoulos@users.noreply.github.com>
2021-03-30 14:49:46 +0300

    Connect the register method to the service
    - Add the registration methods
    - Clean up 'testing' code

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -8,22 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  users: any;
 
-  constructor(private http: HttpClient) { }
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.getUsers();
+   
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
 
-  //get the users from the database and set them to this.users
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(users => this.users = users);
-  }
+
 
   cancelRegisterMode(event: boolean) {
     this.registerMode = event;
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,22 +1,27 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
-  @Input() usersFromHomeComponent: any;
+export class RegisterComponent implements OnInit {  
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   register() {
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe(response => {
+      console.log(response);
+      this.cancel();
+    }, error => {
+      console.log(error);
+    })
   }
 
   cancel() {
