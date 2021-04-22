85eb521c64ce82e437498a9ab80f2881b711dee0
AchilleasTyrnenopoulos <68752518+AchilleasTyrnenopoulos@users.noreply.github.com>
2021-04-22 20:42:25 +0300

    Create an error controller

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,7 +6,9 @@ namespace API.DTOs
     {
         [Required]
         public string Username { get; set; }
+        
         [Required]        
-        public string Password { get; set; }
+        [StringLength(8, MinimumLength = 4)]
+        public string Password { get; set; }       
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -51,9 +51,12 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
+                //exception handling middleware
                 app.UseDeveloperExceptionPage();
+                
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
