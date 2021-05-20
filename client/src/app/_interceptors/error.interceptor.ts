d3d5248213a03583e38397c07de5cc4d5d7a27dd
AchilleasTyrnenopoulos <68752518+AchilleasTyrnenopoulos@users.noreply.github.com>
2021-05-20 20:41:13 +0300

    Error Handling update

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -32,11 +32,11 @@ export class ErrorInterceptor implements HttpInterceptor {
                 //throw this errors back in the component
                 throw modalStateErrors.flat();
               } else {
-                this.toastr.error(error.statusText, error.status);
+                this.toastr.error(error.statusText === 'OK'?"Bad Request": error.statusText, error.status);
               }             
               break;            
             case 401:
-              this.toastr.error(error.statusText, error.status)
+              this.toastr.error(error.statusText === 'OK'?"Unauthorized": error.statusText, error.status)
               break;
             case 404:
               this.router.navigateByUrl('/not-found');
